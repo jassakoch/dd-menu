ee84dd3312a0026070b62ee410cd9676f260debd
paulina koch <pjassakoch@gmail.com>
2025-03-12 15:44:21 -0400

    Added  a nav.js document to inject the nav onto everypage instead of having it on every page

---
+++ nav.js
@@ -0,0 +1,16 @@
+const header = document.createElement("header");
+header.classList.add("header");
+
+header.innerHTML = `
+    <img class="logo" src="images/DOUBLE D’S (500 x 350 px) (500 x 350 px) (500 x 250 px) (1).png" alt="logo">
+    <nav class="menu">
+        <a class="link" href="index.html">Home</a>
+        <a class="link" href="hours.html">Hours</a>
+        <a class="link" href="specials.html">Specials</a>
+        <a class="link" href="https://www.instagram.com/ddroadhouse34?igsh=NXkxYWxrYXJ0bjR4" target="_blank">
+            <i class="fa-brands fa-instagram"></i>
+        </a>
+    </nav>
+`;
+
+document.body.insertAdjacentElement("afterbegin", header);
